c9d64e28a5de606c1ef900a4c814ff052659e62d
Dmitriy Uporov <dyuuporov@avito.ru>
2019-12-27 18:01:11 +0300

    WIP: module modules concept

--- repository-items-favorites/src/main/java/dm/uporov/repository_items_favorites/FavoritesItemsRepositoryCore.kt
+++ repository-items-favorites/src/main/java/dm/uporov/repository_items_favorites/FavoritesItemsRepositoryCore.kt
@@ -2,21 +2,22 @@ package dm.uporov.repository_items_favorites
 
 import android.content.Context
 import dm.uporov.core_favorites.CoreFavorites
-import dm.uporov.core_favorites_api.FavoritesInteractor
+import dm.uporov.core_favorites.coreFavoritesModuleDefinition
 import dm.uporov.machete.annotation.MacheteModule
 import dm.uporov.machete.provider.single
 import dm.uporov.repository_items_api.ItemsRepository
-import dm.uporov.repository_items_favorites.FavoritesItemsRepositoryCoreModuleDefinition.Companion.favoritesItemsRepositoryCoreModuleDefinition
+import dm.uporov.repository_items_favorites.generated.FavoritesItemsRepositoryCoreModuleDefinition.Companion.favoritesItemsRepositoryCoreModuleDefinition
 import dm.uporov.repository_items_favorites_impl.FavoritesItemsRepositoryImpl
 import dm.uporov.repository_items_impl.ItemsRepositoryImpl
 
 @MacheteModule(
     api = [ItemsRepository::class],
-    required = [Context::class, FavoritesInteractor::class],
+    required = [Context::class],
     modules = [CoreFavorites::class]
 )
 object FavoritesItemsRepositoryCore
 
 val favoritesItemsRepositoryModule = favoritesItemsRepositoryCoreModuleDefinition(
-    single { FavoritesItemsRepositoryImpl(ItemsRepositoryImpl(it.getContext()), it.getFavoritesInteractor()) }
+    single { FavoritesItemsRepositoryImpl(ItemsRepositoryImpl(it.getContext()), it.getFavoritesInteractor()) },
+    coreFavoritesModuleDefinition = coreFavoritesModuleDefinition
 )
